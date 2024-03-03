d7aafdd223dd7d411f152234ffd1241588ad2d99
heidi <heidi@worksmate.co.kr>
2024-03-03 22:35:46 +0900

    test. TextField - placeholder test case 추가

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -20,3 +20,31 @@ it('className prop으로 설정한 css class가 적용된다.', async () => {
   // 렌더링되는 DOM 구조가 올바르게 변경되었는 지 확인 → 최종적으로 사용자가 보는 결과는 DOM
   expect(textField).toHaveClass('my-class');
 });
+
+describe('placeholder', () => {
+  it('기본 placeholder "텍스트를 입력해 주세요."가 노출된다.', async () => {
+    // [Arrange] 컴포넌트 렌더링
+    await render(<TextField />);
+
+    // [Act] 이 테스트는 렌더링에 대한 검증이기 때문에 이 단계는 생략한다.
+    // 클릭이나 메서드 호출, prop 변경 등에 대한 작업이 여기에 해당된다.
+
+    // [Assert] 렌더링 후 DOM에 해당 placeholder를 가진 textfield가 존재하는 지 검증
+    const textField = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+    expect(textField).toBeInTheDocument();
+  });
+
+  it('placeholder props에 따라 placeholder가 변경된다,', async () => {
+    const testPlaceholder = '상품명을 입력해주세요';
+
+    // [Arrange] 컴포넌트 렌더링
+    await render(<TextField placeholder={testPlaceholder} />);
+
+    // [Act] 이 테스트는 렌더링에 대한 검증이기 때문에 이 단계는 생략한다.
+    // 클릭이나 메서드 호출, prop 변경 등에 대한 작업이 여기에 해당된다.
+
+    // [Assert] 렌더링 후 DOM에 해당 placeholder를 가진 textfield가 존재하는 지 검증
+    const textField = screen.getByPlaceholderText(testPlaceholder);
+    expect(textField).toBeInTheDocument();
+  });
+});
